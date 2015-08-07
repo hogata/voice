88ecc836e689d5077b929aafac933b5da25da40f
hogata <hiro.ogt@gmail.com>
2015-08-07 11:56:32 +0900

    r

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 (function() {
   var VoiceText, fs, voice;
 
-  require('/app/node_modules/coffee-script/register');
+  require('./node_modules/coffee-script/register');
 
   fs = require('fs');
 
