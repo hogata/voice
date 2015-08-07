eeb967d7ea16ee0fe6769f41b14979aae6458eea
hogata <hiro.ogt@gmail.com>
2015-08-07 09:21:30 +0900

    testtest

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var VoiceText = require 'voicetext'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  voice = new VoiceText('<your api key>')
+voice
+.speaker(voice.SPEAKER.HIKARI)
+.speak 'おはようございます', (e, buf)->
+  console.error e if e
+  fs.writeFile './test.wav', buf, 'binary', (e)->
+    console.error e if e
+    # ./test.wav file generated
+
 });
 
 module.exports = router;
